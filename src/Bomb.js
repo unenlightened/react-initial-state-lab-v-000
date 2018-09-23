d85ea4157d401ce5c904dd08ed9eab4d63717249
Chely Ho <adept_inept@yahoo.com>
2018-09-23 09:43:21 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export default class Bomb extends Component {
-  constructor(state) {
+  constructor(props) {
     super(props)
     this.state = {
       secondsLeft: this.props.initialCount
@@ -9,10 +9,8 @@ export default class Bomb extends Component {
   }
 
   render() {
-   const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
-
    return (
-     <div>{message}</div>
+     <div>{this.state.secondsLeft ? `${this.state.secondsLeft} seconds left before I go boom!` : 'Boom!'}</div>
    )
  }
 
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 export default class ImageSlider extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
     this.state = {
       currentSlideIndex: 0
     }
